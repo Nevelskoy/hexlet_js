031a5eed00652f1abbb73cf199d1b7662e669121
Alex Feliksovich <39111114+Nevelskoy@users.noreply.github.com>
2019-08-21 16:43:01 +0300

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,4 +1,4 @@
-\\ Version LOL
+//Version LOL
 const fizzBuzz = (a, b) => {
   if (a > b) {
     return '';
@@ -6,9 +6,10 @@ const fizzBuzz = (a, b) => {
 (a % 3 === 0 && a % 5 === 0) ? console.log('FizzBuzz') : (a % 3 === 0) ? console.log('Fizz') : (a % 5 === 0) ? console.log('Buzz') : console.log(a);
 return fizzBuzz(a + 1, b); 
 };
+
 export default fizzBuzz;
 
-\\Hexlet version
+//Hexlet version
 const fizzBuzz = (begin, end) => {
   for (let i = begin; i <= end; i += 1) {
     const hasFizz = i % 3 === 0;
