a7f417c390e98e843e05efb4a27ab9ed00668609
Alex Feliksovich <39111114+Nevelskoy@users.noreply.github.com>
2019-08-21 16:41:44 +0300

    Create fizzBuzz.js

---
+++ fizzBuzz.js
@@ -0,0 +1,22 @@
+\\ Version LOL
+const fizzBuzz = (a, b) => {
+  if (a > b) {
+    return '';
+  }
+(a % 3 === 0 && a % 5 === 0) ? console.log('FizzBuzz') : (a % 3 === 0) ? console.log('Fizz') : (a % 5 === 0) ? console.log('Buzz') : console.log(a);
+return fizzBuzz(a + 1, b); 
+};
+export default fizzBuzz;
+
+\\Hexlet version
+const fizzBuzz = (begin, end) => {
+  for (let i = begin; i <= end; i += 1) {
+    const hasFizz = i % 3 === 0;
+    const hasBuzz = i % 5 === 0;
+    const fizzPart = hasFizz ? 'Fizz' : '';
+    const buzzPart = hasBuzz ? 'Buzz' : '';
+    console.log(hasFizz || hasBuzz ? `${fizzPart}${buzzPart}` : i);
+  }
+};
+
+export default fizzBuzz;
