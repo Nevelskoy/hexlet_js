37879a7a379ccc61a42c00a688472f1aa7063bae
Alex Feliksovich <39111114+Nevelskoy@users.noreply.github.com>
2021-01-21 19:50:35 +0300

    Update flatten.js

--- Array/reduce/flatten.js
+++ Array/reduce/flatten.js
@@ -2,3 +2,19 @@ const flatten = (list) => list.reduce((acc, element) => {
   const result = (Array.isArray(element) ? [...acc, ...flatten(element)] : [...acc, element]);
   return result;
 }, []);
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+const flatten = (nested) => {
+  const result = [];
+
+  const iter = (element) => {
+    if (!Array.isArray(element)) {
+      return result.push(element);
+    }
+
+    return element.forEach(iter);
+  };
+
+  iter(nested);
+  return result;
+};
