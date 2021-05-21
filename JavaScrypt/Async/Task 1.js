fd81aa6713bca52032cf2fe1b53e9bf8f91ee616
felix <39111114+Nevelskoy@users.noreply.github.com>
2021-05-21 11:23:16 +0300

    Create Task 1.js

---
+++ JavaScrypt/Async/Task 1.js
@@ -0,0 +1,24 @@
+import path from 'path';
+import fs from 'fs';
+import _ from 'lodash';
+import async from 'async';
+
+// BEGIN
+export const getDirectorySize = (dirpath, cb) => {
+  fs.readdir(dirpath, (error1, filenames) => {
+    if (error1) {
+      cb(error1);
+      return;
+    }
+    const filepaths = filenames.map((name) => path.join(dirpath, name));
+    async.map(filepaths, fs.stat, (error2, stats) => {
+      if (error2) {
+        cb(error2);
+        return;
+      }
+      const sum = _.sumBy(stats.filter((stat) => stat.isFile()), 'size');
+      cb(null, sum);
+    });
+  });
+};
+// END
