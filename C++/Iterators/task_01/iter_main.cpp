7b35e145f93e50a483437e3ec775a8db135a1a60
alex <safelikc@gmail.com>
2024-08-14 20:13:23 +0300

    Update iter_main.cpp

--- C++/Iterators/task_01/iter_main.cpp
+++ C++/Iterators/task_01/iter_main.cpp
@@ -10,33 +10,33 @@ using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
     set<Point> setPoints = { {1,1}, {2,2}, {3,3}, {4,4}, {5,5} };
-	  vector<Point> vecPoints(setPoints.rbegin(), setPoints.rend());
+	vector<Point> vecPoints(setPoints.rbegin(), setPoints.rend());
 
-	  sort(vecPoints.rbegin(), vecPoints.rend());
+	sort(vecPoints.rbegin(), vecPoints.rend());
   	printContainer(vecPoints);
 
-	  //Stream Iterator - ostream_iterator 	
-	  copy(setPoints.begin(), setPoints.end(), std::ostream_iterator<Point>(std::cout, " "));
-	  copy(vecPoints.begin(), vecPoints.end(), std::ostream_iterator<Point>(std::cout, " "));
+	//Stream Iterator - ostream_iterator 	
+	copy(setPoints.begin(), setPoints.end(), std::ostream_iterator<Point>(std::cout, " "));
+	copy(vecPoints.begin(), vecPoints.end(), std::ostream_iterator<Point>(std::cout, " "));
 
   
-	  //Insertion Iterators
-	  //back_inserter()
-	  //front_inserter()  only for list, deque
-	  //inserter() 
+	//Insertion Iterators
+	//back_inserter()
+	//front_inserter()  only for list, deque
+	//inserter() 
 
-	  vector<Point> newVec = { {0,0}, {-1,-1} };
+	vector<Point> newVec = { {0,0}, {-1,-1} };
   	copy(setPoints.begin(), setPoints.end(), back_inserter(vecPoints));
   	printContainer(vecPoints);
 	
 
-	  copy(vecPoints.begin(), vecPoints.end(), inserter(newVec, newVec.end()));
+	copy(vecPoints.begin(), vecPoints.end(), inserter(newVec, newVec.end()));
     printContainer(newVec);
 	
-	  vector<Point> exclPoint = { {-1,-1}, {-2,-2} };
+	vector<Point> exclPoint = { {-1,-1}, {-2,-2} };
 
-	  copy(exclPoint.begin(), exclPoint.end(), inserter(setPoints, setPoints.begin()));
-	  printContainer(setPoints);
+	copy(exclPoint.begin(), exclPoint.end(), inserter(setPoints, setPoints.begin()));
+	printContainer(setPoints);
 	  
     return 0;
 }
