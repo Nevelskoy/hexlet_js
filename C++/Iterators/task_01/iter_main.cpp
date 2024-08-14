c35f585e86ae552f644550a75e0d5d818e3231e9
alex <safelikc@gmail.com>
2024-08-14 20:12:18 +0300

    Create iter_main.cpp

---
+++ C++/Iterators/task_01/iter_main.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <list>
+#include <set>
+#include <iterator>
+#include "Point.h"
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    set<Point> setPoints = { {1,1}, {2,2}, {3,3}, {4,4}, {5,5} };
+	  vector<Point> vecPoints(setPoints.rbegin(), setPoints.rend());
+
+	  sort(vecPoints.rbegin(), vecPoints.rend());
+  	printContainer(vecPoints);
+
+	  //Stream Iterator - ostream_iterator 	
+	  copy(setPoints.begin(), setPoints.end(), std::ostream_iterator<Point>(std::cout, " "));
+	  copy(vecPoints.begin(), vecPoints.end(), std::ostream_iterator<Point>(std::cout, " "));
+
+  
+	  //Insertion Iterators
+	  //back_inserter()
+	  //front_inserter()  only for list, deque
+	  //inserter() 
+
+	  vector<Point> newVec = { {0,0}, {-1,-1} };
+  	copy(setPoints.begin(), setPoints.end(), back_inserter(vecPoints));
+  	printContainer(vecPoints);
+	
+
+	  copy(vecPoints.begin(), vecPoints.end(), inserter(newVec, newVec.end()));
+    printContainer(newVec);
+	
+	  vector<Point> exclPoint = { {-1,-1}, {-2,-2} };
+
+	  copy(exclPoint.begin(), exclPoint.end(), inserter(setPoints, setPoints.begin()));
+	  printContainer(setPoints);
+	  
+    return 0;
+}
